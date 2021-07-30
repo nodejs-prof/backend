8b09c0e1541d06e48ad7c47537f39270f7b87641
DinalieLiyanage <bhagyafdo97@gmail.com>
2021-07-30 15:27:31 +0530

    CORS - handled CORS - enabled notifications

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 // import { DBService } from "./services/db.service";
-
+var cors = require("cors");
+app.use(cors());
 const arg = process.env.ENV || "dev";
 
 const path = `./shared/env-${arg}.json`;
@@ -24,7 +25,7 @@ const { Logger, SEVERITY } = require("./shared/logger");
 const { initializeDatabase } = require("./models/index");
 
 initializeDatabase().then(() => {
-  // require("./schedulers");
+  require("./schedulers");
 });
 
 app.listen(process.env.PORT, () => {
