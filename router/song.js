4e8c3cadbfcca5a5a7a53d84de0cac3061e9667a
AshanChandrasiri <ashanchandrasiri1@gmail.com>
2021-08-04 13:16:38 +0530

    view song

--- router/song.js
+++ router/song.js
@@ -5,7 +5,7 @@ var router = Router();
 
 router.post("/create", SongController.createSong);
 router.get("/all-songs", SongController.getSongs);
-router.get("/song/:id", SongController.getSongsbyId);
+router.get("/view/:id", SongController.getSongsbyId);
 router.delete("/delete/:id", SongController.deleteSong);
 
 export default router;
